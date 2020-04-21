290ef560fbb0aaa0c2e647552fa049c7bfc75c56
Anderson Leite <anderson.leite.bsi@gmail.com>
2020-04-21 14:48:14 -0300

    Definindo a dimensão do palco do jogo

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-alert('teste');
+var height = 0;
+var width  = 0;
+
+function adjustStageSize() {
+    height = window.innerHeight;
+    width  = window.innerWidth;
+    console.log(height, width);
+}
+
+adjustStageSize();
