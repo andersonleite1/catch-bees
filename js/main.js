ccc957dc4516a672babef7e5c0fc2729f40a5253
Anderson Leite <anderson.leite.bsi@gmail.com>
2020-04-22 21:37:29 -0300

    Criando posições radômicas

--- js/main.js
+++ js/main.js
@@ -2,9 +2,32 @@ var height = 0;
 var width  = 0;
 
 function adjustStageSize() {
-    height = window.innerHeight;
     width  = window.innerWidth;
-    console.log(height, width);
+    height = window.innerHeight;
+    
+    console.log(width, height);
 }
 
 adjustStageSize();
+
+function randomPosition() {
+    var positionX = Math.floor(Math.random() * width) - 90;
+    var positionY = Math.floor(Math.random() * height) - 90;
+
+    positionX = positionX < 0 ? 0 : positionX;
+    positionY = positionY < 0 ? 0 : positionY;
+
+    console.log(positionX, positionY);
+
+    // criar o elemento html
+    var bee = document.createElement('img');
+    bee.src = 'img/bee.png';
+    bee.className  = 'bee1';
+    bee.style.left = positionX + 'px';
+    bee.style.top  = positionY + 'px';
+    bee.style.position = 'absolute';
+
+    document.body.appendChild(bee);
+}
+
+
