1892839fd7655d36ea475825417d154fd8a4c6d1
Cleia-Libarino <libarino.cleia.ifba@gmail.com>
2021-06-15 22:07:29 -0300

    Updating the type of variables

--- js/index.js
+++ js/index.js
@@ -1,22 +1,22 @@
 function showHelp() {
-    var btnClose = document.querySelector(".icon-close");
-    var txtDescription = document.querySelector(".info-description");
+    let btnClose = document.querySelector(".icon-close");
+    let txtDescription = document.querySelector(".info-description");
 
     btnClose.classList.add("show");
     txtDescription.classList.add("show");
 }
 
 function closeHelp() {
-    var btnClose = document.querySelector(".icon-close");
-    var txtDescription = document.querySelector(".info-description");
+    let btnClose = document.querySelector(".icon-close");
+    let txtDescription = document.querySelector(".info-description");
 
     btnClose.classList.remove("show");
     txtDescription.classList.remove("show");
 }
 
 function load() { 
-    var btnHelp = document.querySelector(".icon-help");
-    var btnClose = document.querySelector(".icon-close");
+    let btnHelp = document.querySelector(".icon-help");
+    let btnClose = document.querySelector(".icon-close");
 
     btnHelp.addEventListener("click", showHelp, false); 
     btnClose.addEventListener("click", closeHelp, false); 
